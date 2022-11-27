a62a43c0eecb152c08d1263ae6dcd039bec8a0d9
Adil Khan <Adil03320@gmail.com>
2022-11-28 05:00:48 +0530

    Text to speech added to the project

--- app/src/main/java/com/adilkhan/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/adilkhan/a7minutesworkout/ExerciseActivity.kt
@@ -3,11 +3,15 @@ package com.adilkhan.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.adilkhan.a7minutesworkout.databinding.ActivityExerciseBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
     private var exerciseBinding: ActivityExerciseBinding? = null
     // this countDown Time for rest
     private var restTimer : CountDownTimer? = null
@@ -20,11 +24,16 @@ class ExerciseActivity : AppCompatActivity() {
 
     // create exercise list and current position of exercise
     private var exerciseList : ArrayList<ExerciseModel>? = null
-    private var currentPosition = -1
+    private var currentPosition = 0
+
+    // let's add text to speech in exercise
+    // for this create tts
+    private var tts : TextToSpeech? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         exerciseBinding = ActivityExerciseBinding.inflate(layoutInflater)
         setContentView(exerciseBinding?.root)
+
         setSupportActionBar(exerciseBinding?.exerciseToolbar)
         if(supportActionBar!=null)
         {
@@ -33,7 +42,10 @@ class ExerciseActivity : AppCompatActivity() {
         exerciseBinding?.exerciseToolbar?.setNavigationOnClickListener{
             onBackPressed()
         }
+        //initialize exercise list
         exerciseList = Constants.defaultExerciseList()
+        //initialize tts
+        tts = TextToSpeech(this,this)
         setUpRestTimerView()
     }
     private fun setUpRestTimerView()
@@ -41,6 +53,7 @@ class ExerciseActivity : AppCompatActivity() {
         // set visible to rest frame layout and tv title
         exerciseBinding?.flRestProgressBar?.visibility = View.VISIBLE
         exerciseBinding?.tvTitle?.visibility = View.VISIBLE
+        exerciseBinding?.tvNextExercise?.visibility = View.VISIBLE
         // set invisible to image view , text view fl exercise
         exerciseBinding?.flExerciseProgressBar?.visibility = View.INVISIBLE
         exerciseBinding?.tvExerciseTitle?.visibility = View.INVISIBLE
@@ -51,6 +64,8 @@ class ExerciseActivity : AppCompatActivity() {
             restTimer = null
             restProcess = 0
         }
+        exerciseBinding?.tvNextExercise?.text = "Upcoming: \n${exerciseList!![currentPosition].getName()}"
+        speakOut("Take rest for 10 sec")
         setUpRestProgressBar()
     }
     private fun setUpExerciseView()
@@ -58,6 +73,7 @@ class ExerciseActivity : AppCompatActivity() {
         // set invisible to rest frame layout and tv title
         exerciseBinding?.flRestProgressBar?.visibility = View.INVISIBLE
         exerciseBinding?.tvTitle?.visibility = View.INVISIBLE
+        exerciseBinding?.tvNextExercise?.visibility = View.INVISIBLE
         // set visible to image view , text view fl exercise
         exerciseBinding?.flExerciseProgressBar?.visibility = View.VISIBLE
         exerciseBinding?.tvExerciseTitle?.visibility = View.VISIBLE
@@ -69,8 +85,9 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseProcess = 0
         }
         // actual image and text from exercise model to xml
-        exerciseBinding?.ivImage?.setImageResource(exerciseList!![currentPosition].getImage())
-        exerciseBinding?.tvExerciseTitle?.text = exerciseList!![currentPosition].getName()
+        exerciseBinding?.ivImage?.setImageResource(exerciseList!![currentPosition-1].getImage())
+        exerciseBinding?.tvExerciseTitle?.text = exerciseList!![currentPosition-1].getName()
+        speakOut(exerciseList!![currentPosition-1].getName())
         setUpExerciseProgressBar()
     }
     private fun setUpRestProgressBar()
@@ -86,7 +103,7 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "10 finished, let's do some exercise", Toast.LENGTH_LONG).show()
+
                 // when rest finish then call to exercise
                 currentPosition++
                 setUpExerciseView()
@@ -107,10 +124,9 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "30 second finished now let's go for" +
-                        " rest",Toast.LENGTH_LONG).show()
+
                 // when exercise finish then call to rest
-                if(currentPosition<exerciseList!!.size - 1)
+                if(currentPosition<exerciseList!!.size)
                     setUpRestTimerView()
                 else
                 {
@@ -134,7 +150,32 @@ class ExerciseActivity : AppCompatActivity() {
             exerciseTimer?.cancel()
             exerciseTimer = null
         }
+        if(tts!=null)
+        {
+            tts?.stop()
+            tts?.shutdown()
+        }
 
         exerciseBinding = null
+    }
+    // this method text to speech ke interface se implement hua hai
+    override fun onInit(status: Int) {
+         if(status==TextToSpeech.SUCCESS)
+         {
+             val result = tts!!.setLanguage(Locale.US)
+             if(result == TextToSpeech.LANG_NOT_SUPPORTED || result==TextToSpeech.LANG_MISSING_DATA)
+             {
+                 Log.e("TTS", "Specified language not supported")
+             }
+         }
+        else
+         {
+             Log.e("TTS", "initialization failed")
+         }
+    }
+    // this method will speak whatever we will pass as a text
+    private fun speakOut(text:String)
+    {
+        tts!!.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 }
